b4bfc60625e7dc098f005e7057ba37832141197d
qclipsing <alkotikotik@gmail.com>
2026-06-12 14:07:01 +0700

    Verlet might actually work

--- main.asm
+++ main.asm
@@ -2,6 +2,90 @@ section .rodata
     _one: dd 1.0
     _half: dd 0.5
 
+%macro PROCESS_AXIS_VERLET 1
+    ; Load base pointers dynamically based on offset, so 0, 8, 16 for xyz
+    mov r14, [rdi + (%1)]          ; PosX base pointer
+    mov r13, [rdi + 24 + (%1)]     ; VelX base pointer
+    mov r12, [rdi + 48 + (%1)]     ; ForceX base pointer
+
+    mov r11, [rdx + (%1)]          ; PosX output base pointer
+    mov r10, [rdx + 24 + (%1)]     ; VelX out
+    mov r9,  [rdx + 48 + (%1)]     ; AccelX out
+
+    ;;rax is gonna be our "i" in "for" loop
+    xor rax, rax
+    xor r15, r15
+
+%%loop: ;%% means local 
+    cmp r15, rsi
+    jge %%exit
+
+    ; Pos
+    vmovaps zmm0, [r14 + rax] ; 0 to 64 bytes float is 4 bytes so 0-15
+    vmovaps zmm1, [r14 + rax + 64] ; Hence 16 - 32
+    vmovaps zmm2, [r14 + rax + 128] ; And 32 - 48
+    vmovaps zmm3, [r14 + rax + 192]
+
+    ; Vel 
+    vmovaps zmm4, [r13 + rax]
+    vmovaps zmm5, [r13 + rax + 64]
+    vmovaps zmm6, [r13 + rax + 128]
+    vmovaps zmm7, [r13 + rax + 192]
+
+    ; Force 
+    vmovaps zmm8, [r12 + rax]
+    vmovaps zmm9, [r12 + rax + 64]
+    vmovaps zmm10, [r12 + rax + 128]
+    vmovaps zmm11, [r12 + rax + 192]
+    
+    ; Calculate Accel
+    vmulps zmm12, zmm8, zmm30
+    vmulps zmm13, zmm9, zmm30
+    vmulps zmm14, zmm10, zmm30
+    vmulps zmm15, zmm11, zmm30
+    
+    ; Save accel
+    vmovaps [r9 + rax], zmm12
+    vmovaps [r9 + rax + 64], zmm13
+    vmovaps [r9 + rax + 128], zmm14
+    vmovaps [r9 + rax + 192], zmm15
+    
+    ;Calculate new pos
+    vfmadd231ps zmm0, zmm4, zmm31
+    vfmadd231ps zmm1, zmm5, zmm31
+    vfmadd231ps zmm2, zmm6, zmm31
+    vfmadd231ps zmm3, zmm7, zmm31
+
+    vfmadd231ps zmm0, zmm12, zmm29
+    vfmadd231ps zmm1, zmm13, zmm29
+    vfmadd231ps zmm2, zmm14, zmm29
+    vfmadd231ps zmm3, zmm15, zmm29
+
+    ;;Store new pos in r11
+    vmovaps [r11 + rax], zmm0
+    vmovaps [r11 + rax + 64], zmm1
+    vmovaps [r11 + rax + 128], zmm2
+    vmovaps [r11 + rax + 192], zmm3
+    
+    ; Calculate new velocity 
+    vfmadd231ps zmm4, zmm12, zmm31
+    vfmadd231ps zmm5, zmm13, zmm31
+    vfmadd231ps zmm6, zmm14, zmm31
+    vfmadd231ps zmm7, zmm15, zmm31
+    
+    ;Store new velocity in r10
+    vmovaps [r10 + rax], zmm4
+    vmovaps [r10 + rax + 64], zmm5
+    vmovaps [r10 + rax + 128], zmm6
+    vmovaps [r10 + rax + 192], zmm7
+
+    add r15, 64
+    add rax, 256 ; 64 * 4 bytes(per float)
+    jmp %%loop
+
+%%exit:
+%endmacro
+
 section .text
 
 global asm_add_vectors_512 
@@ -10,6 +94,7 @@ asm_add_vectors_512:
 
 .loop:
     ;;According to some convention argv = [rdi, rsi, rdx, rcx, r8, r9] then on stack through xmm0-xmm7
+    ;; Though if any of args are float they jump straight into xmm0-xmm7
     cmp rcx, 0
     jle .exit
 
@@ -40,77 +125,24 @@ verlet_integration:
 ; xmm0 = dt xmm bc float
 ; xmm1 = mass
 ; rdx = verlet outputs
-    vbroadcastss zmm31, xmm0 ;; dt is now all over zmm28
+    vbroadcastss zmm31, xmm0
 
     vbroadcastss zmm29, [_half]
-    vmulps zmm29, zmm29, zmm31
-    vmulps zmm29, zmm29, zmm31 ; dt * 0.5 * dt in zmm31
+    vmulps zmm28, zmm29, zmm31
+    vmulps zmm29, zmm28, zmm31
     
-    ;;Calc 1/mass to then multiply by force which is much more efficient
     vmovss xmm2, [_one]
     vdivss xmm2, xmm2, xmm1
     vbroadcastss zmm30, xmm2
+
+;zmm31 = dt
+;zmm30 = 1/mass 
+;zmm29 = dt * 0.5 * dt 
+;zmm28 = 0.5 * dt
     
-    ;;rax is gonna be our "i" in "for" loop
-    xor rax, rax
-    xor r15, r15
+    ;Clean for every axis
+    PROCESS_AXIS_VERLET 0      ; X Axis
+    PROCESS_AXIS_VERLET 8      ; Y Axis
+    PROCESS_AXIS_VERLET 16     ; Z Axis
 
-.loop:
-    cmp r15, rsi
-    jge .exit
-    
-    ; X 
-    mov r14, [rdi] ; PosX base pointer
-    mov r13, [rdi + 24] ; VelX base pointer
-    mov r12, [rdi + 48] ; AccelX base pointer
-
-    mov r11, [rdx] ; PosX output base pointer
-    mov r10, [rdx + 24] ; Same
-    mov r9, [rdx + 48]
-    
-    ; Pos
-    vmovaps zmm0, [r14 + rax] ; 0 to 64 bytes float is 4 bytes so 0-15
-    vmovaps zmm1, [r14 + rax + 64] ; Hence 16 - 32
-    vmovaps zmm2, [r14 + rax + 128] ; And 32 - 48
-    vmovaps zmm3, [r14 + rax + 192]
-
-    ; Vel 
-    vmovaps zmm4, [r13 + rax]
-    vmovaps zmm5, [r13 + rax + 64]
-    vmovaps zmm6, [r13 + rax + 128]
-    vmovaps zmm7, [r13 + rax + 192]
-
-    ; Accel 
-    vmovaps zmm8, [r12 + rax]
-    vmovaps zmm9, [r12 + rax + 64]
-    vmovaps zmm10, [r12 + rax + 128]
-    vmovaps zmm11, [r12 + rax + 192]
-
-    vmulps zmm8, zmm4, zmm31
-    vmulps zmm9, zmm5, zmm31
-    vmulps zmm10, zmm6, zmm31
-    vmulps zmm11, zmm7, zmm31
-
-    vfmadd231ps zmm0, zmm8, zmm29
-    vfmadd231ps zmm1, zmm9, zmm29
-    vfmadd231ps zmm2, zmm10, zmm29
-    vfmadd231ps zmm3, zmm11, zmm29
-
-    vaddps zmm12, zmm0, zmm8
-    vaddps zmm13, zmm1, zmm9
-    vaddps zmm14, zmm2, zmm10
-    vaddps zmm15, zmm3, zmm11
-
-    vmovaps [r11 + rax], zmm8
-    vmovaps [r11 + rax + 64], zmm9
-    vmovaps [r11 + rax + 128], zmm10
-    vmovaps [r11 + rax + 192], zmm11
-
-
-
-    add r15, 64
-    add rax, 256 ; 64 * 4 bytes(per float)
-    jmp .loop
-
-.exit:
-    ret
+ret
