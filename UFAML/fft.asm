e395752455ea8e4ef90c29739d145120cc5f27c9
qclipsing <alkotikotik@gmail.com>
2026-06-27 23:53:28 +0700

    Loading...

--- UFAML/fft.asm
+++ UFAML/fft.asm
@@ -29,17 +29,94 @@ fft:
     prefetcht1 [r14 + rax + 512]
     prefetcht1 [r13 + rax + 512]
     
-    ;;real
-    vmovaps zmm0, [r14 + rax]
-    vmovaps zmm1, [r14 + rax + 64]
-    vmovaps zmm2, [r14 + rax + 128]
-    vmovaps zmm3, [r14 + rax + 192]
+    ;; So this is very clever trick I recently learnt
+    ;; Instead of doing offset = some_count % stride(rax % r8), which is very slow 
+    ;; I mov r8 into rdx and dec rdx, and then rdx AND rax which gives us rax % r8 
+    mov r15, rax
+    mov rdx, r8
+    dec rdx
+    and r15, rdx
     
-    ;imag
-    vmovaps zmm4, [r13 + rax]
-    vmovaps zmm5, [r13 + rax + 64]
-    vmovaps zmm6, [r13 + rax + 128]
-    vmovaps zmm7, [r13 + rax + 192]
+    ;; Similar thing but opposite 
+    mov rbx, rax
+    not rdx
+    and rbx, rdx
+    
+    ;;Prepare 
+    mov rdx, rbx
+    shl rdx, 4
+    add rdx, r15
+    
+    ;;Load real
+    vmovapd zmm0,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm1,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm2,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm3,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm4,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm5,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm6,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm7,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm8,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm9,  [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm10, [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm11, [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm12, [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm13, [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm14, [r14 + rdx]
+    add rdx, r8
+    vmovapd zmm15, [r14 + rdx]
+    
+    ;;Prepare
+    mov rdx, rbx
+    shl rdx, 4
+    add rdx, r15
+    
+    ;;Load imaginary
+    vmovapd zmm16, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm17, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm18, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm19, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm20, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm21, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm22, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm23, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm24, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm25, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm26, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm27, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm28, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm29, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm30, [r13 + rdx]
+    add rdx, r8
+    vmovapd zmm31, [r13 + rdx]
 
 
     add rax, 256
